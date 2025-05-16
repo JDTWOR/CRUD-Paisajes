5b9000cbfdba9eaa750d38d66afde92c8c92ee52
Tkex555 <bahamutthero@gmail.com>
2025-05-16 08:50:49 -0500

    conexion index

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const path = require('path');
 const connectDB = require('./config/database');
